bb4d2c9b425c4453cc592459ce6c270b53e0163f
Sivarjuen Ravichandran <sivarjuen.ravichandran@hotmail.com>
2021-07-26 12:09:16 +0100

    Add server logic for players joining and leaving lobby

---
+++ Scenes/Instances/Lobby.gd
@@ -0,0 +1,36 @@
+extends Node
+
+var players = []
+var player_names = {}
+
+func add_player(player_id, name):
+	name = name.strip_edges()
+
+	var duplicate_index = 1
+	var original_name = name
+	while name in players:
+		name = original_name.substr(0,8) + " (" + str(duplicate_index) + ")"
+		duplicate_index += 1
+
+	players.append(player_id)
+	player_names[player_id] = name
+
+func remove_player(player_id):
+	var player_index = players.find(player_id)
+	if player_index == -1:
+		return
+
+	players.remove(player_index)
+	player_names.erase(player_id)
+
+func get_ordered_player_names():
+	var names = []
+	for player in players:
+		names.append(player_names[player])
+	return names
+
+func get_player_pos(player_id):
+	return players.find(player_id)
+
+func get_player_ids():
+	return players
--- Scenes/Main/Server.gd
+++ Scenes/Main/Server.gd
@@ -1,23 +1,53 @@
 extends Node
 
 var network = NetworkedMultiplayerENet.new()
-var port = 1909
-var max_players = 100
+var port = 31416
+var max_players = 8
+
+onready var Lobby = get_node("Lobby")
+
+var players = 0
 
 func _ready():
-	StartServer()
+	start_server()
 
-func StartServer():
+func start_server():
 	network.create_server(port, max_players)
 	get_tree().set_network_peer(network)
 	print("Server started")
 
-	network.connect("peer_connected", self, "_Peer_Connected")
-	network.connect("peer_disconnected", self, "_Peer_Disconnected")
+	network.connect("peer_connected", self, "_peer_connected")
+	network.connect("peer_disconnected", self, "_peer_disconnected")
 
-func _Peer_Connected(player_id):
-	print("User " + str(player_id) + " connected")
+func _peer_connected(player_id):
+	if players < 8:
+		players += 1
+		print("Player " + str(player_id) + " connected")
+		rpc_id(player_id, "join_server", players-1)
 
-func _Peer_Disconnected(player_id):
-	print("User " + str(player_id) + " disconnected")
-	
+func _peer_disconnected(player_id):
+	players -= 1
+	remove_from_lobby_local(player_id)
+	print("Player " + str(player_id) + " disconnected")
+
+
+#### LOBBY ####
+remote func add_to_lobby(name):
+	var player_id = get_tree().get_rpc_sender_id()
+	Lobby.add_player(player_id, name)
+	update_lobby()
+
+remote func remove_from_lobby():
+	var player_id = get_tree().get_rpc_sender_id()
+	remove_from_lobby_local(player_id)
+
+func remove_from_lobby_local(player_id):
+	Lobby.remove_player(player_id)
+	update_lobby()
+
+func update_lobby():
+	var names = Lobby.get_ordered_player_names()
+	for player in Lobby.get_player_ids():
+		var pos = Lobby.get_player_pos(player)
+		print(str(player) + " - " + str(pos) + " - " + str(names))
+		rpc_id(player, "update_lobby", pos, names)
